1164547098f3a5c02db37f9650a858f2843226e4
gollanai <aidangollan@icloud.com>
2024-05-19 11:47:19 -0400

    text

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -64,7 +64,7 @@ export const FloatingNav = ({
               "relativeitems-center flex space-x-1 text-neutral-60 hover:text-neutral-500"
             )}
           >
-            <span className="text-sm">{navItem.name}</span>
+            <span className="text-sm text-black">{navItem.name}</span>
           </Link>
         ))}
         <Link href="projects">
